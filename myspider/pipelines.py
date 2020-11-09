caeb8384f0bc2975c3057ac7b5f4c1872907cf4a
wujunjie <2587942508@qq.com>
2020-11-09 11:30:26 +0800

    修改文件存储路径

--- myspider/pipelines.py
+++ myspider/pipelines.py
@@ -13,8 +13,8 @@ import os
 class MyspiderPipeline:
     def process_item(self, item, spider):
         if spider.name == 'douban':
-            douban_result_file_src = os.path.abspath('.') + '/myspider/results/douban.txt'
-            f = open(douban_result_file_src, "a+")
+            douban_result_file_path = os.path.abspath('.') + '/myspider/results/douban.txt'
+            f = open(douban_result_file_path, "a+")
             f.write("文章名：" + item['article_name'] + "\n" + "文章链接：" + item['article_href'] + "\n" + "发帖人昵称：" + item[
                 'user_name'] + "\n" + "发表人主页地址：" + item['user_href'] + "\n" + "最新回应时间：" + item[
                         'last_reply_time'] + "\n" + "评论条数：" + item['comment_count'] + "\n\n\n")
